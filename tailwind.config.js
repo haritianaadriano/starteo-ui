5970e975cb7feade6cff290d47953789b60131af
Abi-Nf <hei.abigail.3@gmail.com>
2024-04-19 11:00:28 -0400

    init app

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: [ 'class' ],
+  content: ['./src/**/*.{html,css,js,jsx,ts,tsx}', './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
