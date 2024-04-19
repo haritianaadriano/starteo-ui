6f933332e755ec04a2f05cddafce8456aec6dde5
Abi-Nf <hei.abigail.3@gmail.com>
2024-04-19 12:11:50 -0400

    feat: auth api provider

---
+++ src/api/provider/auth.ts
@@ -0,0 +1,37 @@
+import { AxiosInstance } from 'axios';
+import { User, UserSignIn, UserSignInResponse, UserSignup } from '@/api';
+
+export class Auth {
+  private bearer: string = '';
+
+  constructor(
+    private client: AxiosInstance
+  ) {}
+
+  getToken(): string {
+    return this.bearer;
+  }
+
+  async me(): Promise<Pick<User, 'id' | 'customization_option'>> {
+    return (await this.client.get(
+      '/auth/whoami',
+      {
+        headers: {
+          Authorization: this.bearer
+        }
+      }
+    )).data;
+  }
+
+  async sign_in(data: UserSignIn): Promise<UserSignInResponse> {
+    const response: UserSignInResponse = (await this.client.post('/auth/signin', data)).data;
+    this.bearer = response.token;
+    return response;
+  }
+
+  async signup(data: UserSignup): Promise<User> {
+    const response: User = (await this.client.post('/auth/signup', data)).data;
+    await this.sign_in({ email: data.email, password: data.password });
+    return response;
+  }
+}
