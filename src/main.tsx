f9753486bf8dbe327f1d9ffa4a02325fb1e405f6
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-04-24 15:14:13 +0300

    refactor: returned type of whoami

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import './assets/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './pages/login';
-import "./assets/style.css"
+import Login from './pages/auth/LoginPage';
+import './assets/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
